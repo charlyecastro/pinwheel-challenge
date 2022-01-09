d9a732a5d157301a4e42dd8f64a1a8ddc326f9b0
charlyecastro <castroc@uw.edu>
2022-01-08 16:40:59 -0800

    updated readme, completet download pdfs method, updated print format

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pinwheel-challenge
+## TODO
+- Finish Up Readme
+- Decide on Case Sensitivity: IDK
+- Decide on Page Navigation for JSON Func: No
+- Decide on Typer: Yes
+- 
--- main.py
+++ main.py
@@ -1,49 +1,67 @@
 import os
 import re
+import json
 import requests
 from bs4 import BeautifulSoup
 from lxml import etree
 from colorama import init, Fore
 
-BASE_URL = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html"
-BASE_SORT_QUERY = "?sortColumn=currentYearRevDate&indexOfFirstRow=0&criteria=formNumber&resultsPerPage=25"
+BASE_URL = "https://apps.irs.gov"
+FORM_PUBLICAITON_URL = BASE_URL + "/app/picklist/list/priorFormPublication.html"
+BASE_SORT_QUERY = "?sortColumn=currentYearRevDate&indexOfFirstRow=0&criteria=formNumber&resultsPerPage=200"
+
+DESC_QUERY = FORM_PUBLICAITON_URL + BASE_SORT_QUERY + "&isDescending=true"
+ASC_QUERY = FORM_PUBLICAITON_URL + BASE_SORT_QUERY + "&isDescending=false"
 
 def main():
     init(autoreset=True)
+    # download_files("Form W-2", 1966, 1971)
+    get_forms_info(["Form W-2G", "Form 720", "W-2"])
 
-    # print(get_forms_info(["Form 11-C", "Form W-2 P"]))
-    find_pdf("Form 707", 1935)
-    # download_files("Form 707", 1935, 1937)
+#  #@, /, &, (, ), *, hyphens, spaces, periods, and commas
+def is_search_acceptable(input: str):
+    match = re.match(r"^[a-zA-Z0-9-@.\/@,()*&\s]*$", input)
+    if (match):
+        return True
+    else:
+        print(Fore.RED + "'" + input + "' contains invalid characters. \n")
+        return False
 
-def is_input_acceptable(input: str):
-    return False # Regex needed
-    #@, /, &, (, ), *, hyphens, spaces, periods, and commas
+def reformat_search(input: str):
+    return "&value=" + input.replace(" ", "+")
+
 
 # returns an array of dictionaries contianing info for each product provided
 def get_forms_info(forms_list: list[str]):
     result = []
     for form in forms_list:
         info = get_form_info(form)
-        result.append(info)
-    return result
-
+        if (info):
+            result.append(info)
+    print("JSON Object for " + str(forms_list) + "\n")
+    print(Fore.CYAN + json.dumps(result, indent=2) + " \n")
+    
 def get_form_info(form_name: str):
-    if (not is_input_acceptable(form_name)):
+    if (not is_search_acceptable(form_name)):
         return
 
-    reformated_form_name = form_name.replace(" ", "+")
-    desc_query = BASE_URL + BASE_SORT_QUERY + "&isDescending=true&value=" + reformated_form_name
-    asc_query = BASE_URL + BASE_SORT_QUERY + "&isDescending=false&value=" + reformated_form_name
-    
+    form_name_query = reformat_search(form_name)
     # Parse descending results
-    desc_page = requests.get(desc_query)
+    desc_page = requests.get(DESC_QUERY + form_name_query)
     desc_soup = BeautifulSoup(desc_page.text, 'html.parser')
 
     # Grab the first desc row that exactly matches the given form_name
-    matching_desc_row = desc_soup.find("a", text=form_name).parent.parent
+    # match = desc_soup.find("a", text=form_name)
+    match = desc_soup.find("a", text=form_name)
 
+    print(match)
+    if (not match):
+        print(Fore.YELLOW + "Could not find information for '" + form_name + "' \n")
+        return None
+
+    matching_desc_row  = match.parent.parent
     # Parse ascending results
-    asc_page = requests.get(asc_query)
+    asc_page = requests.get(ASC_QUERY + form_name_query)
     asc_soup = BeautifulSoup(asc_page.text, 'html.parser')
 
     # Grab the first asc row that exactly matches the given form_name
@@ -63,43 +81,49 @@ def get_form_info(form_name: str):
     }
 
 def download_files(form_name: str, begin_year: int, end_year: int):
-    if (not is_input_acceptable(form_name)):
+    if (not is_search_acceptable(form_name)):
         return
-
-    for y in range(begin_year, end_year + 1):
-        find_pdf(form_name, y)
-
-def find_pdf(form_name: str, year: int):
-    if (not is_input_acceptable(form_name)):
-        print(Fore.RED + "Input '" + form_name + "' contains invalid characters")
-        return
-    # show 200 results per page
-    # check if next page contains a link
-    # make sure list is in ascending order
-    reformated_form_name = form_name.replace(" ", "+")
-    desc_query = BASE_URL + BASE_SORT_QUERY + "&isDescending=true&value=" + reformated_form_name
     
-    # Parse descending results
-    page = requests.get(desc_query)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    query = ASC_QUERY + reformat_search(form_name)
 
-    dom = etree.HTML(str(soup))
+    print(f"looking for '{form_name}' PDF files through years {begin_year} - {end_year}")
+    for y in range(begin_year, end_year + 1):
+        # case insensitive xpath https://stackoverflow.com/questions/2893551/case-insensitive-matching-in-xpath
+        xpath = f"//tr[td/a[text() = '{form_name}']  and td[@class='EndCellSpacer' and contains(text(),'{y}')]]/td/a/@href"
+        pdf_link, url = find_in_pages(xpath, query)
+        if (pdf_link):
+            download_pdf(form_name, y, pdf_link)
+            query = url
+        else:
+            print(Fore.YELLOW + "Could not find'" + form_name + "' for the year " + str(y) + "\n")
 
-    pdf_list = dom.xpath(f"//tr[td/a[text() = '{form_name}']  and td[@class='EndCellSpacer' and contains(text(),'{year}')]]/td/a/@href")
+# Looks for pdf in each page. If pdf found returns pdf link and the url found in. 
+def find_in_pages(xpath: str, url: str): 
+    while (True):
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        dom = etree.HTML(str(soup))
 
-    # if pdf_list:
-    #     return pdf_list[0]
-    # return None
-    pdf_link = pdf_list[0]
-    print(pdf_list)
-    download_pdf(form_name, year, pdf_link)
+        pdf_list = dom.xpath(xpath)
+        # return pdf link and current url if found
+        if (pdf_list):
+            return pdf_list[0], url
+        
+        next_link_list = dom.xpath("//a[text() = 'Next »']/@href")
+        # update url to next link and look for pdf link again
+        if (next_link_list):
+            url = BASE_URL + next_link_list[0]
+        # return none if no more next links
+        else:
+            return None, None
 
-# Downloads a file 
+# Downloads a file from the given url and names it with the given name and year
 def download_pdf(form_name: str, year: int, url: str):   
     folder = "./" + form_name
     file_name = form_name + " - " + str(year) + ".pdf"
 
     if not os.path.exists(folder):
+        print(Fore.CYAN +"Creating new folder '" + folder + "' \n")
         os.mkdir(folder)
 
     response = requests.get(url)
@@ -108,7 +132,7 @@ def download_pdf(form_name: str, year: int, url: str):
     with open(file_path, 'wb') as f:
         f.write(response.content)
     
-    print(Fore.GREEN + "Succefully Downloaded '" + file_name + "'")
+    print(Fore.GREEN + "Succefully Downloaded '" + file_name + "' \n")
 
 if __name__ == "__main__":
     main()
